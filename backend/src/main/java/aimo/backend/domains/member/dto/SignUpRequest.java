dc34a5585f64f4694cd0899f79dd8098f484c781
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-11-02 16:43:05 +0900

    feat: SignUpRequest에 Validation 추가

--- backend/src/main/java/aimo/backend/domains/member/dto/SignUpRequest.java
+++ backend/src/main/java/aimo/backend/domains/member/dto/SignUpRequest.java
@@ -3,12 +3,19 @@ package aimo.backend.domains.member.dto;
 import java.time.LocalDate;
 
 import aimo.backend.domains.member.model.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record SignUpRequest(
+	@NotBlank(message = "userName이 빈 문자열입니다.")
 	String username,
+	@Email(message = "email 형식이 아닙니다.")
 	String email,
+	@NotBlank(message = "password가 빈 문자열입니다.")
 	String password,
 	Gender gender,
-	String phoneNumber,
-	LocalDate birth) {
+	@NotBlank(message = "phoneNumber이 빈 문자열입니다.")
+	String phone_number,
+	LocalDate birth
+) {
 }
