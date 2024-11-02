33484c2e13c22fc98f0c7f751b8e3ce0d9151c64
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-11-02 16:01:08 +0900

    feat: Login 가짜 controller 추가

--- backend/src/main/java/aimo/backend/domains/member/controller/MemberController.java
+++ backend/src/main/java/aimo/backend/domains/member/controller/MemberController.java
@@ -1,26 +1,29 @@
 package aimo.backend.domains.member.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import aimo.backend.domains.member.dto.DeleteRequest;
-import aimo.backend.domains.member.dto.SignUpRequest;
 import aimo.backend.common.dto.DataResponse;
 import aimo.backend.domains.auth.security.jwtFilter.JwtTokenProvider;
-import aimo.backend.util.memberLoader.MemberLoader;
+import aimo.backend.domains.member.dto.DeleteRequest;
+import aimo.backend.domains.member.dto.SignUpRequest;
 import aimo.backend.domains.member.service.MemberService;
+import aimo.backend.util.memberLoader.MemberLoader;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -41,7 +44,7 @@ public class MemberController {
 	}
 
 	@PostMapping("/signup")
-	public ResponseEntity<DataResponse<Void>> signupMember(@RequestBody SignUpRequest signUpRequest) {
+	public ResponseEntity<DataResponse<Void>> signupMember(@RequestBody @Valid SignUpRequest signUpRequest) {
 		memberService.signUp(signUpRequest);
 
 		return ResponseEntity
@@ -58,5 +61,14 @@ public class MemberController {
 		return ResponseEntity
 			.status(HttpStatus.NO_CONTENT)
 			.body(DataResponse.noContent());
+	}
+
+	@PostMapping(value = "/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<DataResponse<Void>> loginMember(
+		@RequestParam("email") String email,
+		@RequestParam("password") String password
+	) {
+		// 실제로 실행되지 않고 Swagger 문서용도로만 사용됩니다.
+		return ResponseEntity.ok(DataResponse.ok());
 	}
 }
