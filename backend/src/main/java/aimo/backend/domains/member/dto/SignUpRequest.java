1ca8bcc01f665c6e038048f427d5e4135596e6c2
mng990 <ryoo0504@gmail.com>
2024-11-04 06:56:00 +0900

    fix: 오탈자 수정

--- backend/src/main/java/aimo/backend/domains/member/dto/SignUpRequest.java
+++ backend/src/main/java/aimo/backend/domains/member/dto/SignUpRequest.java
@@ -5,16 +5,18 @@ import java.time.LocalDate;
 import aimo.backend.domains.member.model.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 public record SignUpRequest(
 	@NotBlank(message = "userName이 빈 문자열입니다.")
-	String username,
+	String memberName,
 	@Email(message = "email 형식이 아닙니다.")
 	String email,
 	@NotBlank(message = "password가 빈 문자열입니다.")
 	String password,
-  @NotNull(message = "gender가 비었습니다.")
+  	@NotNull(message = "gender가 비었습니다.")
 	Gender gender,
-  @Past(message = "생년월일은 과거 날짜여야 합니다.")
+  	@Past(message = "생년월일은 과거 날짜여야 합니다.")
 	LocalDate birth
 ) {}
