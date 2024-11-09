b4ef70ca66c581d53543c4fd304667c49a0d8bbd
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-11-09 15:37:46 +0900

    refactor: Post 목록 조회 최신순 조회로 변경

--- backend/src/main/java/aimo/backend/domains/post/service/PostService.java
+++ backend/src/main/java/aimo/backend/domains/post/service/PostService.java
@@ -4,21 +4,17 @@ import static aimo.backend.common.exception.ErrorCode.*;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.util.Pair;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import aimo.backend.common.exception.ApiException;
 import aimo.backend.common.mapper.PostMapper;
-import aimo.backend.common.mapper.PrivatePostMapper;
-import aimo.backend.domains.comment.entity.ChildComment;
 import aimo.backend.domains.comment.entity.ParentComment;
 import aimo.backend.domains.member.entity.Member;
 import aimo.backend.domains.post.dto.requset.FindCommentedPostsByIdRequest;
@@ -29,7 +25,6 @@ import aimo.backend.domains.post.dto.response.FindPostsByPostTypeResponse;
 import aimo.backend.domains.post.entity.Post;
 import aimo.backend.domains.post.model.PostType;
 import aimo.backend.domains.post.repository.PostRepository;
-import aimo.backend.domains.privatePost.dto.response.JudgementResponse;
 import aimo.backend.domains.privatePost.service.PrivatePostService;
 import aimo.backend.util.memberLoader.MemberLoader;
 import lombok.RequiredArgsConstructor;
@@ -84,15 +79,16 @@ public class PostService {
 	) {
 		Page<FindPostsByPostTypeResponse> posts;
 		Member member = memberLoader.getMember();
+		Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
 
 		if (postType == PostType.MY) {
-			posts = findMyPosts(member.getId(), PageRequest.of(page, size));
+			posts = findMyPosts(member.getId(), pageable);
 		} else if (postType == PostType.POPULAR) {
-			posts = findPopularPosts(PageRequest.of(page, size));
+			posts = findPopularPosts(pageable);
 		} else if (postType == PostType.COMMENTED) {
-			posts = findCommentedPosts(member.getId(), PageRequest.of(page, size));
+			posts = findCommentedPosts(member.getId(), pageable);
 		} else {
-			posts = findAnyPosts(PageRequest.of(page, size));
+			posts = findAnyPosts(pageable);
 		}
 
 		posts.forEach(post -> log.info("post: {}", post));
@@ -131,10 +127,9 @@ public class PostService {
 			.forEach((p) -> {
 				FindCommentedPostsByIdRequest commentedPost = PostMapper.toFindCommentedPostsByIdRequest(p);
 				int index = commentedPosts.indexOf(commentedPost);
-				if(index != -1 && commentedPosts.get(index).commentedAt().isAfter(p.getCreatedAt())) {
+				if (index != -1 && commentedPosts.get(index).commentedAt().isAfter(p.getCreatedAt())) {
 					commentedPosts.set(index, commentedPost);
-				}
-				else {
+				} else {
 					commentedPosts.add(commentedPost);
 				}
 			});
@@ -146,10 +141,10 @@ public class PostService {
 		int end = Math.min((start + pageable.getPageSize()), commentedPosts.size());
 
 		List<FindPostsByPostTypeResponse> pagedPosts = commentedPosts
-				.subList(start, end)
-				.stream()
-				.map(PostMapper::toFindPostsByPostTypeResponse)
-				.toList();
+			.subList(start, end)
+			.stream()
+			.map(PostMapper::toFindPostsByPostTypeResponse)
+			.toList();
 
 		// Page 객체 생성
 		return new PageImpl<>(pagedPosts, pageable, commentedPosts.size());
