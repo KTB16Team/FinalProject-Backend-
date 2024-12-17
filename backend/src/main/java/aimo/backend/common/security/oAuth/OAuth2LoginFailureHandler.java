d4851348eb0d136acc3d64e9a76b2e61eb84ff7f
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-12-17 09:31:19 +0900

    refactor: OAuth 로그인 실패시 500 페이지로 리다이렉팅 및 로그 출력

--- backend/src/main/java/aimo/backend/common/security/oAuth/OAuth2LoginFailureHandler.java
+++ backend/src/main/java/aimo/backend/common/security/oAuth/OAuth2LoginFailureHandler.java
@@ -8,24 +8,34 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationFa
 import org.springframework.stereotype.Component;
 
 import aimo.backend.common.dto.DataResponse;
+import aimo.backend.common.properties.FrontProperties;
 import aimo.backend.common.util.responseWriter.ResponseWriter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+	private final FrontProperties frontProperties;
 
 	@Override
 	public void onAuthenticationFailure(
 		HttpServletRequest request,
 		HttpServletResponse response,
 		AuthenticationException exception
-	) {
-
+	) throws IOException {
 		log.error("OAuth2 authentication failed", exception);
 
-		ResponseWriter.writeResponse(response, DataResponse.ok(), HttpStatus.OK);
+		// React의 Redirect URI로 리다이렉트
+		String redirectUrl = String.format(
+			"%s/500",
+			frontProperties.getDomain()
+		);
+
+		getRedirectStrategy().sendRedirect(request, response, redirectUrl);
 	}
 }
