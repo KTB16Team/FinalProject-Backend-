aa4e179ffef5eacbffbd06eba518f3d8440de99f
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-12-18 17:21:56 +0900

    feat: 회원가입 기능 구현

--- backend/src/main/java/aimo/backend/domains/email/controller/EmailController.java
+++ backend/src/main/java/aimo/backend/domains/email/controller/EmailController.java
@@ -25,30 +25,30 @@ import lombok.RequiredArgsConstructor;
 public class EmailController {
 
 	private final EmailService emailService;
-	//
-	// @PostMapping("/verification-request/signup")
-	// @Operation(
-	// 	summary = "회원가입 email 인증 요청",
-	// 	description = "email 인증을 위한 이메일을 전송",
-	// 	responses = {
-	// 		@ApiResponse(
-	// 			responseCode = "200",
-	// 			description = "성공"
-	// 		),
-	// 		@ApiResponse(
-	// 			responseCode = "409",
-	// 			description = "이미 가입된 이메일입니다.",
-	// 			content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-	// 		)
-	// 	}
-	// )
-	// public ResponseEntity<DataResponse<Void>> sendSignupVerificationCode(
-	// 	@RequestBody @Valid SendVerificationCodeRequest request
-	// ) {
-	// 	emailService.validateDuplicatedEmailAndSendEmailVerification(request);
-	//
-	// 	return ResponseEntity.ok(DataResponse.ok());
-	// }
+
+	@PostMapping("/verification-request/signup")
+	@Operation(
+		summary = "회원가입 email 인증 요청",
+		description = "email 인증을 위한 이메일을 전송",
+		responses = {
+			@ApiResponse(
+				responseCode = "200",
+				description = "성공"
+			),
+			@ApiResponse(
+				responseCode = "409",
+				description = "이미 가입된 이메일입니다.",
+				content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+			)
+		}
+	)
+	public ResponseEntity<DataResponse<Void>> sendSignupVerificationCode(
+		@RequestBody @Valid SendVerificationCodeRequest request
+	) {
+		emailService.validateDuplicatedEmailAndSendEmailVerification(request);
+
+		return ResponseEntity.ok(DataResponse.ok());
+	}
 
 	@PostMapping("/verification-request/password-reissue")
 	@Operation(
@@ -68,37 +68,4 @@ public class EmailController {
 
 		return ResponseEntity.ok(DataResponse.ok());
 	}
-
-	// @PostMapping("/verification")
-	// @Operation(
-	// 	summary = "email 인증 확인",
-	// 	description = """
-	// 		인증 요청을 한 email이 없으면 401
-	// 		code가 일치하면 true, 일치하지 않으면 false를 반환한다.""",
-	// 	responses = {
-	// 		@ApiResponse(
-	// 			responseCode = "200",
-	// 			description = "성공"
-	// 		),
-	// 		@ApiResponse(
-	// 			responseCode = "400",
-	// 			description = "유효하지 않은 이메일 코드입니다.",
-	// 			content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-	// 		),
-	// 		@ApiResponse(
-	// 			responseCode = "401",
-	// 			description = "이메일 인증을 시도해주세요.",
-	// 			content = @Content(schema = @Schema(implementation = ErrorResponse.class))
-	// 		)
-	// 	}
-	// )
-	// public ResponseEntity<DataResponse<VerifyEmailCodeResponse>> verifyEmailCode(
-	// 	@RequestBody @Valid VerifyEmailCodeRequest request
-	// ) {
-	// 	VerifyEmailCodeParameter parameter = new VerifyEmailCodeParameter(request.getEmail(), request.getCode());
-	//
-	// 	VerifyEmailCodeResponse response = emailService.verifyEmailCode(parameter);
-	//
-	// 	return ResponseEntity.ok(DataResponse.from(response));
-	// }
 }
--- backend/src/main/java/aimo/backend/domains/member/dto/parameter/SignUpParameter.java
+++ backend/src/main/java/aimo/backend/domains/member/dto/parameter/SignUpParameter.java
@@ -2,29 +2,21 @@ package aimo.backend.domains.member.dto.parameter;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import aimo.backend.domains.member.dto.request.SignUpRequest;
 import aimo.backend.domains.member.model.Gender;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 
 public record SignUpParameter(
-	@NotBlank(message = "닉네임이 필요합니다.")
+	Integer code,
 	String nickname,
-	@Email(message = "이메일 형식이 아닙니다.")
 	String email,
-	@NotBlank(message = "비밀번호가 필요합니다.")
-	@Min(value = 6, message = "비밀번호는 6자 이상이어야 합니다.")
 	String password,
 	Gender gender,
-	@JsonFormat(pattern = "yyyy-MM-dd")
 	LocalDate birth
 ) {
 
 	public static SignUpParameter from(SignUpRequest request) {
 		return new SignUpParameter(
+			request.code(),
 			request.nickname(),
 			request.email(),
 			request.password(),
