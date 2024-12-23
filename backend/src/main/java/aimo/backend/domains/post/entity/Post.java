927ea4c626c297cdd01bb348cffa30ad9d9ff146
BOOK-U3FJG82JE7\USER <hl3470@naver.com>
2024-12-23 22:10:44 +0900

    feat: Post에 인덱싱 적용

--- backend/src/main/java/aimo/backend/domains/post/entity/Post.java
+++ backend/src/main/java/aimo/backend/domains/post/entity/Post.java
@@ -29,6 +29,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
@@ -39,8 +40,13 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "posts")
 @NoArgsConstructor(access = PROTECTED)
+@Table(name = "posts",
+	indexes = {
+		@Index(name = "posts_idx_member_id", columnList = "member_id"),
+		@Index(name = "posts_idx_post_views_count", columnList = "post_views_count"),
+	}
+)
 public class Post extends BaseEntity {
 
 	@Id
